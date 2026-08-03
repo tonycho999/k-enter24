e93c63881c8a92e0859af7530f8b6198f4cbab45
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-08-03 13:15:15 +0800

    Create AdRight.tsx

---
+++ src/components/AdRight.tsx
@@ -0,0 +1,11 @@
+// src/components/AdRight.tsx
+export default function AdRight() {
+  return (
+    <aside className="ad-sidebar">
+      {/* 
+        추후 여기에 구글 애드센스 스크립트를 넣으세요. (수직형 광고)
+      */}
+      Right Ad (160x600)
+    </aside>
+  );
+}
