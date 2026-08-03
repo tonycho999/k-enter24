caf9188ae063bb6b4f6944df8b0afa6f355b0bda
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-08-03 13:14:28 +0800

    Create AdTop.tsx

---
+++ src/components/AdTop.tsx
@@ -0,0 +1,12 @@
+// src/components/AdTop.tsx
+export default function AdTop() {
+  return (
+    <div className="ad-banner-top">
+      {/* 
+        추후 여기에 구글 애드센스 스크립트를 넣으세요.
+        예: <ins className="adsbygoogle" style={{ display: 'block' }} data-ad-client="ca-pub-XXXXXX" ... /> 
+      */}
+      Google AdSense Top Banner (728x90)
+    </div>
+  );
+}
