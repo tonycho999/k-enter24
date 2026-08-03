63b44ddf6fd3849c4de4b1b56e24067507a06ec5
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-08-03 12:52:09 +0800

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,9 +5,11 @@ export default function Sidebar() {
     <aside className="sidebar">
       <Link href="/" className="logo">K-ENTER 24</Link>
       <nav>
-        <Link href="/k-pop" className="nav-link">K-Pop</Link>
-        <Link href="/k-drama" className="nav-link">K-Drama</Link>
-        <Link href="/k-culture" className="nav-link">K-Culture</Link>
+        <Link href="/k-pop" className="nav-link">K-POP</Link>
+        <Link href="/k-drama" className="nav-link">K-DRAMA</Link>
+        <Link href="/k-movie" className="nav-link">K-MOVIE</Link>
+        <Link href="/k-entertainment" className="nav-link">K-ENTERTAINMENT</Link>
+        <Link href="/k-culture" className="nav-link">K-CULTURE</Link>
       </nav>
     </aside>
   );
