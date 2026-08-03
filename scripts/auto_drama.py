6432de0fd3b85609f2382ccb8f7e2fb4d8067e51
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-08-03 16:29:46 +0800

    Update auto_drama.py

--- scripts/auto_drama.py
+++ scripts/auto_drama.py
@@ -8,7 +8,7 @@ UNSPLASH_ACCESS_KEY = os.environ.get("UNSPLASH_ACCESS_KEY")
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 CATEGORY_NAME = "K-DRAMA"
-SEARCH_QUERY = "K드라마"
+SEARCH_QUERY = "드라마"
 
 # 1️⃣ [신규 추가] DB에서 최근 작성한 글 제목 5개를 가져오는 함수
 def get_recent_titles():
