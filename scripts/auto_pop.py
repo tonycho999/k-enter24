ce8d3788cd96665ca0efcc853adbff7fb9e4890e
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-08-03 14:54:24 +0800

    Update auto_pop.py

--- scripts/auto_pop.py
+++ scripts/auto_pop.py
@@ -8,7 +8,7 @@ UNSPLASH_ACCESS_KEY = os.environ.get("UNSPLASH_ACCESS_KEY")
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 CATEGORY_NAME = "K-POP"
-SEARCH_QUERY = "K팝"
+SEARCH_QUERY = "아이돌, 가수, 가요, 음악"
 
 # 1️⃣ [신규 추가] DB에서 최근 작성한 글 제목 5개를 가져오는 함수
 def get_recent_titles():
