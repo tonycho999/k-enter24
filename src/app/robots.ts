450e1e0ca19b5ce89a6984802162177e4dc97c59
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-08-03 14:57:55 +0800

    Create robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+// src/app/robots.ts
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://k-enter24.com/sitemap.xml',
+  };
+}
