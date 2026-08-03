0537ad017e74812b53ce6857dec1bf12b5e66f03
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-08-03 13:18:11 +0800

    Update AdLeft.tsx

--- src/components/AdLeft.tsx
+++ src/components/AdLeft.tsx
@@ -1,11 +1,4 @@
 // src/components/AdLeft.tsx
 export default function AdLeft() {
-  return (
-    <aside className="ad-sidebar">
-      {/* 
-        추후 여기에 구글 애드센스 스크립트를 넣으세요. (수직형 광고)
-      */}
-      Left Ad (160x600)
-    </aside>
-  );
+  return null;
 }
